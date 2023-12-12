026038acdb3e9723918c50e9fc1bc45ea231a05a
“itNapominki” <it.napominki.mos@gmail.com>
2023-12-12 12:40:34 +0300

    доработал отправку данных в бота

--- src/App.js
+++ src/App.js
@@ -1,78 +1,99 @@
-// import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
-import {
-  // createBrowserRouter,
-  // RouterProvider,
-  // Routes,
-  // Route,
-} from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import {} from "react-router-dom";
 
 const tg = window.Telegram.WebApp;
 
 function App() {
-  useEffect(() => {
-    tg.ready();
-    tg.MainButton.show();
-  }, []);
+ 
 
   const onClose = () => {
     tg.close();
   };
 
+  function Form() {
+
+    useEffect(() => {
+      tg.ready();
+      tg.MainButton.show();
+      tg.MainButton.setParams({ text: "Отправить данные" });
+    }, []);
+
+
+
+
+
+
+    const [name, setName] = useState();
+    const [phone, setPhone] = useState();
+
+
+    // данные в телеграмм
+    const onSendData = useCallback(() => {
+      const data = {
+        name,
+        phone,
+      }
+tg.sendData(JSON.stringify(data))
+    }, [])
+
+
+    useEffect(()=> {
+      tg.onEvent("mainButtonClicked", onSendData)
+      return () => {
+        tg.offEvent("mainButtonClicked", onSendData)
+      }
+
+    }, [])
+
+    const onChangeName = (e) => {
+      setName(e.target.value);
+    }
+    
+    const onChangePhone = (e) => {
+      setPhone(e.target.value);
+    }
+
+
+
+
+
+
+    return (
+      <>
+        <input
+          className="input"
+          type="text"
+          placeholder="Ваше имя"
+          required
+          value={name}
+          onChange={onChangeName}
+        ></input>        
+        <input
+          className="input"
+          type="text"
+          placeholder="Ваш телефон"
+          required
+          value={phone}
+          onChange={onChangePhone}
+        ></input>
+      </>
+    );
+  }
+
   return (
     <div className="App">
       <div className="wrapper_input">
-        {/* <Routes>
-          <Route
-            path="/frontend-for-bot/login"
-            element={
-              <>
-                <h2>Запоните форму заявку</h2>
-                <input className="input" placeholder="Имя заказчика"></input>
-                <input
-                  className="input"
-                  placeholder="Телефон заказчика"
-                ></input>
-                <input className="input" placeholder="ФИО умершего"></input>
-                <input className="input" placeholder="Место прощания"></input>
-                <input className="input" placeholder="Район проживания"></input>
-                <input className="input" placeholder="Дата поминок"></input>
-                <input className="input" placeholder="Время прощания"></input>
-                <input
-                  className="input"
-                  placeholder="Количество человек"
-                ></input>
-                <input className="input" placeholder="Комментарий"></input>
-                <div className="button_container">
-                  <button className="button" onClick={onClose}>
-                    Отменить
-                  </button>
-                  <button className="button" onClick={onClose}>
-                    Отправить
-                  </button>
-                </div>
-              </>
-            }
-          />
-          <Route path="/frontend-for-bot/auth" element={<></>} />
-        </Routes> */}
-
-        <>
-          <h2>Заполните форму регистрации</h2>
-          <input className="input" placeholder="Ваше имя" required></input>
-          <input className="input" placeholder="Ваша Фамилия" required></input>
-          <input className="input" placeholder="Ваш телефон" required></input>
-
-          <div className="button_container">
-            <button className="button" onClick={onClose}>
-              Отменить
-            </button>
-            <button className="button" onClick={onClose}>
+        <h2>Заполните форму регистрации</h2>
+        <Form />
+        <div className="button_container">
+          <button className="button" onClick={onClose}>
+            Отменить
+          </button>
+          {/* <button className="button" onClick={onClose}>
               Отправить
-            </button>
-          </div>
-        </>
+            </button> */}
+        </div>
       </div>
     </div>
   );
